1dc03a1bd000700f2d39b3bf8e7b6b1305fd4da1
Jacek Leja <jacek4leja@gmail.com>
2019-12-03 22:44:39 +0100

    Continued development of 'Distrustful' AI.

--- players/distrustful.py
+++ players/distrustful.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from framework import BasePlayer, Choice, ChoiceDetails
+from framework import BasePlayer, Choice, ChoiceDetails, utils
 
 
 class Distrustful(BasePlayer):
@@ -10,8 +10,13 @@ class Distrustful(BasePlayer):
 
     @staticmethod
     def check_for_play(self, round_info, player_number):
+        if player_number is round_info.player_turn:
+            player_hand = round_info.player_hand
+        else:
+            player_hand = utils.get_player_hand_by_number(round_info, player_number)
+
         best_card = 0
-        for card in round_info.player_hand:
+        for card in player_hand:
             if card.is_playable(round_info):
                 if card.revealed_rank is 1 or card.reavealed_rank > best_card:
                     best_card = card.hand_position
@@ -24,17 +29,18 @@ class Distrustful(BasePlayer):
         return False
 
     @staticmethod
-    def check_for_necessary_tip(self, round_info, player_number):
-        return False
-
-    @staticmethod
     def check_for_obvious_discard(self, round_info, player_number):
+        if player_number is round_info.player_turn:
+            player_hand = round_info.player_hand
+        else:
+            player_hand = utils.get_player_hand_by_number(round_info, player_number)
+
         lowest_rank = min(round_info.board_state.values())
-        for card in round_info.player_hand:
-            if (card.revealed_rank is not None and card.revealed_rank < lowest_rank) or \
-               (card.revealed_suit is not None and round_info.board_state[card.revealed_suit] is 5) or \
-               (card.revealed_suit is not None and card.revealed_rank is not None and
-                    round_info.board_state[card.revealed_suit] >= card.revealed_rank):
+        for card in player_hand:
+            if (card.revealed_rank is not None and card.revealed_rank <= lowest_rank) or \
+                    (card.revealed_suit is not None and round_info.board_state[card.revealed_suit] is 5) or \
+                    (card.revealed_suit is not None and card.revealed_rank is not None and
+                     round_info.board_state[card.revealed_suit] >= card.revealed_rank):
                 return ChoiceDetails(
                     Choice.DISCARD,
                     card.hand_position
@@ -42,7 +48,39 @@ class Distrustful(BasePlayer):
         return False
 
     @staticmethod
-    def check_for_tip(self, round_info):
+    def check_for_guess_discard(self, round_info, player_number):
+        if player_number is round_info.player_turn:
+            player_hand = round_info.player_hand
+        else:
+            player_hand = utils.get_player_hand_by_number(round_info, player_number)
+        unmarked = []
+        for card in player_hand:
+            if card.revealed_rank is None and card.reavealed_suit is None:
+                unmarked.append(card)
+        if len(unmarked) is 0:
+            return False
+        else:
+            oldest = unmarked[0]
+            for card in unmarked:
+                if card.draw_on_turn < oldest.draw_on_turn:
+                    oldest = card
+            return ChoiceDetails(
+                Choice.DISCARD,
+                oldest.hand_position
+            )
+        return False
+
+    @staticmethod
+    def check_for_necessary_tip(self, round_info, player_number):
+        if player_number is round_info.player_turn:
+            player_hand = round_info.player_hand
+        else:
+            player_hand = utils.get_player_hand_by_number(round_info, player_number)
+
+        return False
+
+    @staticmethod
+    def check_for_good_tip(self, round_info):
         return False
 
     def play(self, round_info):
@@ -50,6 +88,6 @@ class Distrustful(BasePlayer):
                         self.check_for_good_tip, self.check_for_guess_discard]
 
         for action in action_order:
-            decision = action(round_info)
+            decision = action(round_info, round_info.player_turn)
             if decision is not False:
                 return decision
